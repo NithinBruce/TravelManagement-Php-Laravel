05f91c5639c1dd4b09394224a81e2c419ee94e30
Nithin <nithinbruce07@gmail.com>
2024-05-06 10:54:26 +0530

    update

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('products',[App\Http\Controllers\ApiController::class,'getProducts']);
     Route::get('products/{id}/details',[App\Http\Controllers\ApiController::class,'getProductDetails']);
 Route::post('products/{id}/update',[App\Http\Controllers\ApiController::class,'updateProductDetails']);
-Route::get('products/{id}/delete',[App\Http\Controllers\ApiController::class,'deleteProductDetails']);
+Route::delete('products/{id}/delete',[App\Http\Controllers\ApiController::class,'deleteProductDetails']);
 });
 
 Route::post('signup',[App\Http\Controllers\AuthController::class,'signup']);
